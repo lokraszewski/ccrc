27c43c368fdb38ffe606fcb520a915bc7a7b256c
Lukasz Okraszewski <lu@localhost.localdomain>
2018-01-26 20:28:26 +0000

    Added a basic google benchmark to build system. Need to actually define the benchmark now.

--- src/bitop.hpp
+++ src/bitop.hpp
@@ -27,28 +27,28 @@ template<class T> T reverse(T n)
 
 template<> uint32_t reverse(uint32_t n)
 {
-	n = (n >> 1) & 0x55555555 | (n << 1) & 0xaaaaaaaa;
-	n = (n >> 2) & 0x33333333 | (n << 2) & 0xcccccccc;
-	n = (n >> 4) & 0x0f0f0f0f | (n << 4) & 0xf0f0f0f0;
-	n = (n >> 8) & 0x00ff00ff | (n << 8) & 0xff00ff00;
-	n = (n >> 16) & 0x0000ffff | (n << 16) & 0xffff0000;
+	n = ((n >> 1) & 0x55555555) | ((n << 1) & 0xaaaaaaaa);
+	n = ((n >> 2) & 0x33333333) | ((n << 2) & 0xcccccccc);
+	n = ((n >> 4) & 0x0f0f0f0f) | ((n << 4) & 0xf0f0f0f0);
+	n = ((n >> 8) & 0x00ff00ff) | ((n << 8) & 0xff00ff00);
+	n = ((n >> 16) & 0x0000ffff) | ((n << 16) & 0xffff0000);
 	return n;
 }
 
 template<> uint16_t reverse(uint16_t n)
 {
-	n = (n >> 1) & 0x5555 | (n << 1) & 0xaaaa;
-	n = (n >> 2) & 0x3333 | (n << 2) & 0xcccc;
-	n = (n >> 4) & 0x0f0f | (n << 4) & 0xf0f0;
-	n = (n >> 8) & 0x00ff | (n << 8) & 0xff00;
+	n = ((n >> 1) & 0x5555) | ((n << 1) & 0xaaaa);
+	n = ((n >> 2) & 0x3333) | ((n << 2) & 0xcccc);
+	n = ((n >> 4) & 0x0f0f) | ((n << 4) & 0xf0f0);
+	n = ((n >> 8) & 0x00ff) | ((n << 8) & 0xff00);
 	return n;
 }
 
 template<> uint8_t reverse(uint8_t n)
 {
-	n = (n >> 1) & 0x55 | (n << 1) & 0xaa;
-	n = (n >> 2) & 0x33 | (n << 2) & 0xcc;
-	n = (n >> 4) & 0x0f | (n << 4) & 0xf0;
+	n = ((n >> 1) & 0x55) | ((n << 1) & 0xaa);
+	n = ((n >> 2) & 0x33) | ((n << 2) & 0xcc);
+	n = ((n >> 4) & 0x0f) | ((n << 4) & 0xf0);
 	return n;
 }
 
