d16cc79adcaab35ea341f601b4480e5d9bba4d26
Lukasz Okraszewski <lu@localhost.localdomain>
2018-03-11 18:36:22 +0000

    First iteration of crc fast. Going to add constexpr next

--- benchmark/bm.cpp
+++ benchmark/bm.cpp
@@ -2,7 +2,7 @@
 * @Author: lu
 * @Date:   25-01-2018
 * @Last Modified by:   lu
-* @Last Modified time: 26-01-2018
+* @Last Modified time: 11-03-2018
 */
 
 #include <benchmark/benchmark.h>
@@ -25,14 +25,17 @@
 #include <thread>
 #include <utility>
 #include <vector>
-
 #include "cpp-crc.hpp"
 
 static void BM_slow_crc32(benchmark::State &state)
 {
 	using namespace cpp_crc::slow;
 	uint8_t *data = new uint8_t[state.range(0)];
-	memset(data, 'x', state.range(0));
+
+	for (auto i = 0; i < state.range(0); ++i)
+	{
+		data[i] = rand();
+	}
 
 	crc<uint32_t> c32(0x4C11DB7, 0xFFFFFFFF, 0xFFFFFFFF, true, true );
 
@@ -49,7 +52,11 @@ static void BM_slow_crc8(benchmark::State &state)
 {
 	using namespace cpp_crc::slow;
 	uint8_t *data = new uint8_t[state.range(0)];
-	memset(data, 'x', state.range(0));
+
+	for (auto i = 0; i < state.range(0); ++i)
+	{
+		data[i] = rand();
+	}
 
 	crc<uint8_t> c8(0x07, 0x00, 0x00, false, false);
 
@@ -61,5 +68,48 @@ static void BM_slow_crc8(benchmark::State &state)
 	delete[] data;
 }
 BENCHMARK(BM_slow_crc8)->RangeMultiplier(2)->Range(8, 1<<18)->Complexity(benchmark::oN);
+
+static void BM_fast_crc32(benchmark::State &state)
+{
+	using namespace cpp_crc::fast;
+	uint8_t *data = new uint8_t[state.range(0)];
+
+	for (auto i = 0; i < state.range(0); ++i)
+	{
+		data[i] = rand();
+	}
+
+	crc<uint32_t> c32(0x4C11DB7, 0xFFFFFFFF, 0xFFFFFFFF, true, true );
+
+	for (auto _ : state)
+		benchmark::DoNotOptimize(c32.get(data, state.range(0)));
+
+	state.SetComplexityN(state.range(0));
+
+	delete[] data;
+}
+BENCHMARK(BM_fast_crc32)->RangeMultiplier(2)->Range(8, 1<<18)->Complexity(benchmark::oN);
+
+static void BM_fast_crc8(benchmark::State &state)
+{
+	using namespace cpp_crc::fast;
+	uint8_t *data = new uint8_t[state.range(0)];
+
+	for (auto i = 0; i < state.range(0); ++i)
+	{
+		data[i] = rand();
+	}
+
+	crc<uint8_t> c8(0x07, 0x00, 0x00, false, false);
+
+	for (auto _ : state)
+		benchmark::DoNotOptimize(c8.get(data, state.range(0)));
+
+	state.SetComplexityN(state.range(0));
+
+	delete[] data;
+}
+BENCHMARK(BM_fast_crc8)->RangeMultiplier(2)->Range(8, 1<<18)->Complexity(benchmark::oN);
+
 
 BENCHMARK_MAIN();
--- src/cpp-crc.hpp
+++ src/cpp-crc.hpp
@@ -35,7 +35,11 @@ protected:
 	const CRC_TYPE TOP_BIT;
 
 public:
-	crc(const CRC_TYPE poly, const CRC_TYPE seed = 0, const CRC_TYPE XorOut = 0, const bool RefIn = false,  const bool RefOut = false) :
+	crc(const CRC_TYPE poly,
+		const CRC_TYPE seed = 0,
+		const CRC_TYPE XorOut = 0,
+		const bool RefIn = false,
+		const bool RefOut = false) :
 		m_poly(poly),
 		m_seed(seed),
 		m_xor(XorOut),
@@ -67,7 +71,7 @@ public:
 
 		while (len--)
 		{
-			if(m_ref_in)
+			if (m_ref_in)
 			{
 				crc ^= (bitop::reverse(*data++) << (WIDTH - 8));
 			}
@@ -75,7 +79,6 @@ public:
 			{
 				crc ^= (*data++ << (WIDTH - 8));
 			}
-
 
 			for (auto bit = 8; bit--;)
 			{
@@ -88,10 +91,9 @@ public:
 					crc = crc << 1;
 				}
 			}
-
 		}
 
-		if(m_ref_out)
+		if (m_ref_out)
 		{
 			return (bitop::reverse(crc)) ^ m_xor;
 		}
@@ -106,7 +108,115 @@ public:
 
 namespace fast
 {
+template<typename CRC_TYPE>
+class crc
+{
+private:
+	void init_table(void)
+	{
+		for (auto d = 0; d < 0x100; ++d)
+		{
+			/*
+			 * Start with the dividend followed by zeros.
+			 */
+			CRC_TYPE rem = d << (WIDTH - 8);
 
+			/*
+			 * Perform modulo-2 division, a bit at a time.
+			 */
+			for (auto bit = 8; bit--;)
+			{
+				/*
+				 * Try to divide the current data bit.
+				 */
+				if (rem & TOP_BIT)
+				{
+					rem = (rem << 1) ^ m_poly;
+				}
+				else
+				{
+					rem = (rem << 1);
+				}
+			}
+
+			/*
+			 * Store the result into the table.
+			 */
+			m_table[d] = rem;
+		}
+	}
+protected:
+	const CRC_TYPE m_poly;
+	const CRC_TYPE m_seed;
+	const CRC_TYPE m_xor;
+
+	const bool m_ref_in;
+	const bool m_ref_out;
+	const size_t WIDTH;
+	const CRC_TYPE TOP_BIT;
+
+	CRC_TYPE  m_table[0x100];
+
+public:
+	crc(const CRC_TYPE poly,
+		const CRC_TYPE seed = 0,
+		const CRC_TYPE XorOut = 0,
+		const bool RefIn = false,
+		const bool RefOut = false) :
+		m_poly(poly),
+		m_seed(seed),
+		m_xor(XorOut),
+		m_ref_in(RefIn),
+		m_ref_out(RefOut),
+		WIDTH(8 * sizeof(CRC_TYPE)),
+		TOP_BIT((1 << (WIDTH - 1)))
+	{
+		init_table();
+	}
+
+	const CRC_TYPE get_seed() const
+	{
+		return m_seed;
+	}
+
+	const CRC_TYPE get_poly() const
+	{
+		return m_poly;
+	}
+
+	CRC_TYPE get(uint8_t *data, uint32_t len)
+	{
+		return get(data, len, m_seed);
+	}
+
+	CRC_TYPE get(uint8_t *data, uint32_t len, const CRC_TYPE seed)
+	{
+		CRC_TYPE crc = seed;
+
+		while (len--)
+		{
+			if (m_ref_in)
+			{
+				*data = bitop::reverse(*data) ^ (crc >> (WIDTH - 8));
+				crc = m_table[*data++] ^ (crc << 8);
+			}
+			else
+			{
+				*data = (*data) ^ (crc >> (WIDTH - 8));
+				crc = m_table[*data++] ^ (crc << 8);
+			}
+		}
+
+		if (m_ref_out)
+		{
+			return (bitop::reverse(crc)) ^ m_xor;
+		}
+		else
+		{
+			return crc ^ m_xor;
+		}
+	}
+};
 }
 
 }
